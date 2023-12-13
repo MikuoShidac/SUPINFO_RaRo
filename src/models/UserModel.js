f576e95473a3c5062d67f5d133c7f821601485f0
Shidac <avie2204@gmail.com>
2023-12-13 22:28:09 +0100

    IL EST EN VIE git add .git add . handicapé mais en vie

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { passportLocalMongoose } from "passport-local-mongoose";
+import passportLocalMongoose from "passport-local-mongoose";
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
